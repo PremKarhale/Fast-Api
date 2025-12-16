6806a8a7c0a27079e03d343d1e33fa2720023c06
Prem Karhale <premkarhale383@gmail.com>
2025-12-17 02:37:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,18 @@ def hello():
 def about():
     return {'message':'Here we design fully functional API for your Patient Records Managemnt'}
 
+
+#Data of all the patients 
 @app.get('/view')
 def view():
     data = loadData()
     return data
+
+#Data of specific patient
+@app.get('/patient/{patient_id}')
+
+def view_patient(patient_id:str):
+    data=loadData() #load all data
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'There is no such Patient exits of this ID '}
