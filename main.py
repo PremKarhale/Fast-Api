f6a7b2c76807159d5cba87e0e72c484b4a935f3b
Prem Karhale <premkarhale383@gmail.com>
2025-12-17 02:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+import json
+
+def loadData():
+    with open('patient.json','r',encoding="utf-8") as f:
+        data=json.load(f)
+    return data
 
 app = FastAPI()
 
 @app.get("/")  # 1st route define 
 def hello():
-    return {'message':'Hello World'}
+    return {'message':'Patient Management System API'}
 
 @app.get('/about') #2nd route 
 def about():
-    return {'message':'campus X is the coding platform hosted by nitish sir '}
+    return {'message':'Here we design fully functional API for your Patient Records Managemnt'}
+
+@app.get('/view')
+def view():
+    data = loadData()
+    return data
