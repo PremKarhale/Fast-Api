903cbc69039c6b4179c117e91e38cca6705abc34
Prem Karhale <premkarhale383@gmail.com>
2025-12-20 00:50:07 +0530

    Update Pydentic2.py

--- Pydentic2.py
+++ Pydentic2.py
@@ -1,22 +1,29 @@
-from pydantic import BaseModel
+## Pydentic class is uded to check the text error or Data validation error 
+
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
 from typing import List,Dict,Optional
 
+#Pydentic class
 class Patient(BaseModel):
-    name:str
-    age:int
-    weight:float 
+    #Fields
+    name:str=Field(max_length=50)
+    age:int=Field(gt=0 , lt=120)
+    Email:EmailStr
+    Linkdin_ID:AnyUrl
+    weight:float=Field(gt=0)
     married:Optional[bool] = False # default set 
-    allergies:Optional[List[str]]= False # default set 
+    allergies:Optional[List[str]]= None # default set 
     contact_details:Dict[str,str]
 
 def insert_patient_data(patient:Patient):
     print(patient.name)
     print(patient.age)
     print(patient.allergies)
-    print(patient.contact_details['email']) # always access dictionary by [] and class using dot . !!
+    print(patient.Email)
+    print(patient.contact_details['mob_no.']) # always access dictionary by [] and class using dot . !!
 
 
-patient_info={'name':'prem','age':23,'weight':60,'married':True,'contact_details':{'email':'prem@1234','mob_no.':'234547484'}}
+patient_info={'name':'prem','Email':'abc34@gmail.com','Linkdin_ID':'https://linkedin.com/322','age':23,'weight':60,'married':True,'contact_details':{"mob_no.":'234547484'}}
 patient1=Patient(**patient_info)
 
 insert_patient_data(patient1)
