f5410333eb3546ab94a9dd16759ed5761f9c684d
Prem Karhale <premkarhale383@gmail.com>
2026-01-14 00:03:54 +0530

    Update patients.py

--- routes/patients.py
+++ routes/patients.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter 
 from fastapi import FastAPI,Path,HTTPException 
 from fastapi.responses import JSONResponse
-from schemas import Patient
+from schemas import Patient , PatientUpdate
 import json 
 
 
@@ -47,9 +47,67 @@ def create_patient(patient:Patient):
         raise HTTPException (status_code=400 , detail="Patient already exist")
     
     # add new Patient 
-    data[patient.id]=patient.model_dump(exclude=['id']) # converts from pydentic obj to dictonary
+    data[patient.id] = patient.model_dump(exclude=['id']) # converts from pydentic obj to dictonary
     
     # save the data into patient.json 
     saveData(data)
 
-    return JSONResponse(status_code=201, content={"message":"Patinet added Successfully !!"})
+    return JSONResponse(
+        status_code=201,
+        content={"message": "Patient added Successfully !!",
+                 "Patient_data":data[patient.id]}
+    )
+
+
+
+# PATCH :Update
+@router.patch("/{id}")
+def updatePatient(id:str, patient_update:PatientUpdate):
+    try:
+        data = loadData()
+        if id not in data:
+            raise HTTPException (status_code = 400 , detail = "Patient not found !")
+        
+        existing_patient_info=data[id]
+        updated_patient_info =patient_update.model_dump(exclude_unset=True)
+        for key , value in updated_patient_info.items():
+            existing_patient_info[key] = value
+        
+        # to get the bmi or verdict
+        # existing_patinet_info -> pydentic object ->update bmi or verdict 
+        # -> pydantic obj -> dict 
+
+        existing_patient_info["id"]=id
+        patient_pydantic_obj = Patient(** existing_patient_info) #gives pydentic obj
+        existing_patient_info=patient_pydantic_obj.model_dump(exclude=['id']) # this creates dict
+
+        data[id]=existing_patient_info
+
+        # saves data 
+        saveData(data)
+        return JSONResponse(status_code=200 , content = {"message":"data updated Sucessfully!",
+                                                        "updated_data":data[id]})
+    except Exception as e :
+        raise HTTPException( status_code=500 , detail=f"{e}" )
+
+    
+#delete 
+@router.delete("/{patient_id}")
+def delete_patient(patient_id:str=Path(...,description='ID of Patient to be deleted',example='P001')):
+    data = loadData()
+    if  patient_id not in data:
+        raise HTTPException(status_code=404 , detail="patient not found !")
+    delete_patient = data.pop(patient_id)
+
+    saveData(data)
+
+    return JSONResponse(
+        status_code= 200 , 
+        content = {"message":f"Patient  is deleted Sucesssfully !",
+                   "deleted_id":f"{patient_id}",
+                   "deleted_data":delete_patient
+                   }
+    )
+
+    
+    
