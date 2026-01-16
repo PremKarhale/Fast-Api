52b5317df17baf5f7040cbd2343cef8087a083b1
Prem Karhale <premkarhale383@gmail.com>
2026-01-17 00:26:03 +0530

    Create model.py

---
+++ routes/model.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter,HTTPException
+import pickle
+import pandas as pd 
+from schemas import userInput
+from fastapi.responses import JSONResponse
+
+#importing ml model 
+with open("model.pkl" , "rb") as f:
+    model = pickle.load(f) # this created my model
+
+router = APIRouter()
+
+#Predict end point 
+@router.post("/predict")
+def predict_premimum(data: userInput):
+    try:
+        input_df=pd.DataFrame([{
+        'income_lpa':data.income_lpa,  #input data in the dataframe
+        'occupation':data.occupation,
+        'age_group':data.age_group,
+        'BMI':data.BMI,
+        'lifeStyleRisk':data.lifeStyle_risk,
+        'Tier_cities':data.city_tier
+    }])
+        prediction = model.predict(input_df)[0]
+        prediction = prediction.item() 
+
+        return JSONResponse(status_code=200 , content={"pridected_catagory":prediction})
+    except Exception as e :
+        raise HTTPException(status_code=500,detail=f"{e}")
+
+
+  
